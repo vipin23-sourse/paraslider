2cee4b517c78ccd9af5f5b6a4f70d1dc061213b5
vipin vijayakumar <vipin23.mobiiworld@gmail.com>
2021-09-13 15:28:58 +0530

    updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,10 +2,10 @@
 
 $(document).ready(function () {
   $('#fullpage').fullpage({
-    navigation: true,
+    navigation: false,
     navigationPosition: 'right',
     dragAndMove: true,
-    navigationTooltips: ['section1', 'section2', 'section3', 'section4'],
+    // navigationTooltips: ['section1', 'section2','section3','section4',],1
     showActiveTooltip: true,
     slidesNavigation: true,
     slidesNavPosition: 'bottom',
